89ae7a5e3ac8d3d92c8d5d781415042c6098a94a
rntvilela <renatovilela55@gmail.com>
2023-06-21 22:26:39 -0300

    Add win check

--- js/scripts.js
+++ js/scripts.js
@@ -5,18 +5,11 @@ const Gameboard = {
 	x : 0,
 	o : 0,
 	turn : false,
+	mode : false
 }
 
-const restart = () => {
+const clearboard = () => {
 	const cells = document.querySelectorAll('.cell');
-	const xscore = document.getElementById('x-score');
-	const oscore = document.getElementById('o-score');
-
-	Gameboard.x = 0;
-	Gameboard.o = 0;
-
-	xscore.textContent = `X - ${Gameboard.x}`;
-	oscore.textContent = `O - ${Gameboard.o}`;
 	
 	for (let i = 0; i < cells.length; i++) {
 		Gameboard.board[i] = "";
@@ -24,11 +17,65 @@ const restart = () => {
 	}
 }
 
+const updatescore = (op, value) => {
+	const xscore = document.getElementById('x-score');
+	const oscore = document.getElementById('o-score');
+	
+	if (op === 1) {
+		Gameboard.x = value;
+		xscore.textContent = `X - ${Gameboard.x}`;
+	}
+	else if (op === 2) {
+		Gameboard.o = value;
+		oscore.textContent = `O - ${Gameboard.o}`;
+	}
+	else {
+		Gameboard.x = value;
+		xscore.textContent = `X - ${Gameboard.x}`;
+		Gameboard.o = value;
+		oscore.textContent = `O - ${Gameboard.o}`;
+	}
+}
 
+const restart = () => {
+	Gameboard.turn = false;
+	Gameboard.mode = false;
+	Gameboard.board = ["", "", "", "", "", "", "", "", ""];
+	updatescore(0, 0);
+	clearboard();
+}
+
+const checkwin = () => {
+	for (let i = 0; i < 9; i+=3) {
+		if (Gameboard.board[i] && ((Gameboard.board[i] === Gameboard.board[i + 1]) && (Gameboard.board[i] === Gameboard.board[i + 2]))) return true;
+	}	
+	
+	for (let i = 0; i < 3; i++) {
+		if (Gameboard.board[i] && ((Gameboard.board[i] === Gameboard.board[i + 3]) && (Gameboard.board[i] === Gameboard.board[i + 6]))) return true;
+	}
+	
+	if (Gameboard.board[0] && ((Gameboard.board[0] === Gameboard.board[4]) && (Gameboard.board[0] === Gameboard.board[8]))) return true;
+
+	if (Gameboard.board[2] && ((Gameboard.board[2] === Gameboard.board[4]) && (Gameboard.board[2] === Gameboard.board[6]))) return true;
+
+	return false
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	const cells = document.querySelectorAll('.cell');
 	const rst_btn = document.getElementById('restart');
+	const pvp_btn = document.getElementById('pvp');
+	const pva_btn = document.getElementById('pva');
+
+	restart();
+
+	pvp_btn.addEventListener('click', () => {
+		Gameboard.mode = false;
+	});
+	
+	pva_btn.addEventListener('click', () => {
+		Gameboard.mode = true;
+	});
 
 	rst_btn.addEventListener('click', () => {
 		restart();
@@ -41,15 +88,17 @@ document.addEventListener('DOMContentLoaded', () => {
 			if (!Gameboard.turn) mark = "X";
 			else mark = "O";
 
-			Gameboard.board[i] = mark;
-			cells[i].textContent = Gameboard.board[i];
-			Gameboard.turn = !Gameboard.turn;
-
-			checkwin();
+			if (!Gameboard.board[i]) {
+				Gameboard.board[i] = mark;
+				cells[i].textContent = Gameboard.board[i];
+				Gameboard.turn = !Gameboard.turn;
+				if (checkwin()) {
+					if (Gameboard.turn) updatescore(1, Gameboard.x + 1);
+					else updatescore(2, Gameboard.o + 1); 
+					Gameboard.turn = false;
+					clearboard();
+				}
+			}
 		});
 	}
-
-
-
-
 });
