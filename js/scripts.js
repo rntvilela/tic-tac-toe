86d77ca85e49871a74d5b6be8c8951a461207c35
rntvilela <renatovilela55@gmail.com>
2023-06-24 15:15:13 -0300

    Add IA move

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-const Gameboard = {
+const Tictactoe = {
 	board : [
 		["", "", "", "", "", "", "", "", ""],
 	],
@@ -6,77 +6,85 @@ const Gameboard = {
 	o : 0,
 	turn : false,
 	mode : false,
-	move : 0
-}
+	move : 0,
 
-const clearboard = () => {
-	const cells = document.querySelectorAll('.cell');
-	
-	for (let i = 0; i < cells.length; i++) {
-		Gameboard.board[i] = "";
-		cells[i].textContent = Gameboard.board[i];
+	clearboard : () => {
+		const cells = document.querySelectorAll('.cell');
+
+		for (let i = 0; i < cells.length; i++) {
+			Tictactoe.board[i] = "";
+			cells[i].textContent = Tictactoe.board[i];
+		}
+	},
+
+	updatescore : (op, value) => {
+		const xscore = document.getElementById('x-score');
+		const oscore = document.getElementById('o-score');
+
+		if (op === 1) {
+			Tictactoe.x = value;
+			xscore.textContent = `X - ${Tictactoe.x}`;
+		}
+		else if (op === 2) {
+			Tictactoe.o = value;
+			oscore.textContent = `O - ${Tictactoe.o}`;
+		}
+		else {
+			Tictactoe.x = value;
+			xscore.textContent = `X - ${Tictactoe.x}`;
+			Tictactoe.o = value;
+			oscore.textContent = `O - ${Tictactoe.o}`;
+		}
+	},
+
+	display : (msg) => {
+		const board = document.querySelector('.board');
+		const result = document.querySelector('.result');
+
+		board.style.display = "none";
+		result.style.display = "flex";
+
+		result.textContent = msg;
+
+		setTimeout( () => {
+			board.style.display = "grid";
+			result.style.display = "none";
+		}, 1500);
+	},
+
+	restart : (score) => {
+		Tictactoe.turn = false;
+		Tictactoe.mode = false;
+		Tictactoe.move = 0;
+		Tictactoe.board = ["", "", "", "", "", "", "", "", ""];
+		if (score) Tictactoe.updatescore(0, 0);
+		Tictactoe.clearboard();
+	},
+
+	checkwin : () => {
+		for (let i = 0; i < 9; i+=3) {
+			if (Tictactoe.board[i] && ((Tictactoe.board[i] === Tictactoe.board[i + 1]) && (Tictactoe.board[i] === Tictactoe.board[i + 2]))) return true;
+		}	
+
+		for (let i = 0; i < 3; i++) {
+			if (Tictactoe.board[i] && ((Tictactoe.board[i] === Tictactoe.board[i + 3]) && (Tictactoe.board[i] === Tictactoe.board[i + 6]))) return true;
+		}
+
+		if (Tictactoe.board[0] && ((Tictactoe.board[0] === Tictactoe.board[4]) && (Tictactoe.board[0] === Tictactoe.board[8]))) return true;
+
+		if (Tictactoe.board[2] && ((Tictactoe.board[2] === Tictactoe.board[4]) && (Tictactoe.board[2] === Tictactoe.board[6]))) return true;
+
+		return false
+	},
+
+	ia : () => {
+		i = parseInt(Math.random() * 9);
+
+		while
+
 	}
 }
 
-const updatescore = (op, value) => {
-	const xscore = document.getElementById('x-score');
-	const oscore = document.getElementById('o-score');
-	
-	if (op === 1) {
-		Gameboard.x = value;
-		xscore.textContent = `X - ${Gameboard.x}`;
-	}
-	else if (op === 2) {
-		Gameboard.o = value;
-		oscore.textContent = `O - ${Gameboard.o}`;
-	}
-	else {
-		Gameboard.x = value;
-		xscore.textContent = `X - ${Gameboard.x}`;
-		Gameboard.o = value;
-		oscore.textContent = `O - ${Gameboard.o}`;
-	}
-}
-
-const display = (msg) => {
-	const board = document.querySelector('.board');
-	const result = document.querySelector('.result');
-
-	board.style.display = "none";
-	result.style.display = "flex";
-
-	result.textContent = msg;
-	
-	setTimeout( () => {
-		board.style.display = "grid";
-		result.style.display = "none";
-	}, 2000);
-}
-
-const restart = (score) => {
-	Gameboard.turn = false;
-	Gameboard.mode = false;
-	Gameboard.move = 0;
-	Gameboard.board = ["", "", "", "", "", "", "", "", ""];
-	if (score) updatescore(0, 0);
-	clearboard();
-}
-
-const checkwin = () => {
-	for (let i = 0; i < 9; i+=3) {
-		if (Gameboard.board[i] && ((Gameboard.board[i] === Gameboard.board[i + 1]) && (Gameboard.board[i] === Gameboard.board[i + 2]))) return true;
-	}	
-	
-	for (let i = 0; i < 3; i++) {
-		if (Gameboard.board[i] && ((Gameboard.board[i] === Gameboard.board[i + 3]) && (Gameboard.board[i] === Gameboard.board[i + 6]))) return true;
-	}
-	
-	if (Gameboard.board[0] && ((Gameboard.board[0] === Gameboard.board[4]) && (Gameboard.board[0] === Gameboard.board[8]))) return true;
-
-	if (Gameboard.board[2] && ((Gameboard.board[2] === Gameboard.board[4]) && (Gameboard.board[2] === Gameboard.board[6]))) return true;
-
-	return false
-}
 
 document.addEventListener('DOMContentLoaded', () => {
 	const cells = document.querySelectorAll('.cell');
@@ -84,48 +92,52 @@ document.addEventListener('DOMContentLoaded', () => {
 	const pvp_btn = document.getElementById('pvp');
 	const pva_btn = document.getElementById('pva');
 
-	restart();
+	Tictactoe.restart();
 
 	pvp_btn.addEventListener('click', () => {
-		Gameboard.mode = false;
+		Tictactoe.mode = false;
 	});
-	
+
 	pva_btn.addEventListener('click', () => {
-		Gameboard.mode = true;
+		Tictactoe.mode = true;
 	});
 
 	rst_btn.addEventListener('click', () => {
-		restart(1);
+		Tictactoe.restart(1);
 	});
-	
+
 
 	let mark;
 	for (let i = 0; i < cells.length; i++) {
 		cells[i].addEventListener('click', () => {
-			if (!Gameboard.turn) mark = "X";
+			if (!Tictactoe.turn) mark = "X";
 			else mark = "O";
 
-			if (!Gameboard.board[i]) {
-				Gameboard.board[i] = mark;
-				cells[i].textContent = Gameboard.board[i];
-				Gameboard.turn = !Gameboard.turn;
-				Gameboard.move++;
-				if (Gameboard.move >= 5) {
-					if (checkwin()) {
-						if (Gameboard.turn) {
-							updatescore(1, Gameboard.x + 1);
-							display("X wins!");
-							restart();
+			if (!Tictactoe.board[i]) {
+				Tictactoe.board[i] = mark;
+				cells[i].textContent = Tictactoe.board[i];
+				Tictactoe.turn = !Tictactoe.turn;
+				Tictactoe.move++;
+				if (Tictactoe.move >= 5) {
+					if (Tictactoe.checkwin()) {
+						if (Tictactoe.turn) {
+							Tictactoe.updatescore(1, Tictactoe.x + 1);
+							Tictactoe.display("X wins!");
+							Tictactoe.restart();
 						}
 						else {
-							updatescore(2, Gameboard.o + 1);
-							display("O wins!");
-							restart();
+							Tictactoe.updatescore(2, Tictactoe.o + 1);
+							Tictactoe.display("O wins!");
+							Tictactoe.restart();
 						}
-					} else if (Gameboard.move == 9) {
-						display("It's a tie!");
-						restart();
+					} else if (Tictactoe.move == 9) {
+						Tictactoe.display("It's a tie!");
+						Tictactoe.restart();
 					}
+				}
+
+				if (Tictactoe.mode) {
+					Tictactoe.ia();
 				}
 			}
 		});
